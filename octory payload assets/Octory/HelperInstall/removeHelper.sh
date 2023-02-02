f932b60971d7e17cde3eaaeff26f19ffa8c3017d
Raell Dottin <raell.dottin@proton.me>
2023-02-02 06:48:23 -0500

    remove miscellaneous directories

--- octory payload assets/Octory/HelperInstall/removeHelper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-# removeHelper v1.0.0
-# Amaris (Alexis Bridoux)
-
-/bin/launchctl unload /Library/LaunchDaemons/com.amaris.octory.helper.plist
-/bin/rm /Library/LaunchDaemons/com.amaris.octory.helper.plist
-/bin/rm /Library/PrivilegedHelperTools/com.amaris.octory.helper
